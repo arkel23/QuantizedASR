9a6f76374c7391c0f242cd33568af35afd3611e8
ubuntu <calvinku1209@gmail.com>
2026-02-04 13:00:00 +0800

    test_long_ml

---
+++ scripts/test_long_ml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dataset_long_paths=("espnet/floras" "espnet/floras" "espnet/floras" "espnet/floras" "espnet/floras" "espnet/floras")
+
+dataset_configs=("multilingual_ar" "multilingual_ar" "multilingual_es" "multilingual_es" "multilingual_eng" "multilingual_en")
+
+dataset_splits=("dev" "test" "dev" "test" "dev" "test")
+
+base_cmd="python -m tools.evaluate --serial 999 --warmup_steps 2 --max_eval_samples 4 --batch_size 2 --long_form --long_form_tricks --model openai/whisper-tiny"
+
+# Iterate through all combinations
+for i in "${!dataset_paths[@]}"; do
+    # Execute the command
+    cmd="$base_cmd --dataset_path ${dataset_paths[$i]} --dataset ${configs[$i]} --split ${splits[$i]}"
+    echo "Running: $cmd"
+    $cmd
+done
