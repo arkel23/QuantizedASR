8bec6e5de8b9dd8e54325f420d6982988ba2db25
ubuntu <calvinku1209@gmail.com>
2026-02-13 18:25:07 +0800

    set up eval

--- scripts/eval_short_zh.sh
+++ scripts/eval_short_zh.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-CONFIG_FILES=(
+DATASET_CONFIGS=(
     "uea_aishell1.yaml"
-    "uea_kespeech.yaml"
     "uea_wenetspeech_meeting.yaml"
     "uea_wenetspeech_net.yaml"
     "uea_cmmlu.yaml"
@@ -17,16 +16,47 @@ CONFIG_FILES=(
     "mcv19_zhtw_test.yaml"
     "taiwan_tongues_zhtw.yaml"
     "taiwan_tongues_hokkien.yaml"
-    "taiwan_tongues_hakka.yaml"
     "uea_cv15_yue.yaml"
 )
 
-base_cmd="python -m tools.evaluate --serial 999 --batch_size 4"
+    # does not work in hpc / no standardized tones romanization for hakka
+    # "uea_kespeech.yaml"
+    # "taiwan_tongues_hakka.yaml"
+
+MODEL_CONFIGS=(
+    "whisper_tiny.yaml"
+    "whisper_small.yaml"
+    "whisper_base.yaml"
+    "whisper_medium.yaml"
+    "whisper_large.yaml"
+    "whisper_large_v2.yaml"
+    "whisper_large_v3.yaml"
+    "whisper_large_v3_turbo.yaml"
+
+    "lite_whisper_large_v3_acc.yaml"
+    "lite_whisper_large_v3.yaml"
+    "lite_whisper_large_v3_fast.yaml"
+    "lite_whisper_large_v3_turbo_acc.yaml"
+    "lite_whisper_large_v3_turbo.yaml"
+    "lite_whisper_large_v3_turbo_fast.yaml"
+
+    "voxtral_mini_3b.yaml"
+    "voxtral_small_24b.yaml"
+    "granite_speech_2b.yaml"
+    "granite_speech_8b.yaml"
+    "qwen_25_omni_7b.yaml"
+    "qwen_2_audio_7b.yaml"
+    "qwen_2_audio_7b_instruct.yaml"
+)
+
+base_cmd="python -m tools.evaluate --serial 100 --batch_size 4"
 
 # Iterate through all combinations
-for cfg in "${CONFIG_FILES[@]}"; do
-    # Execute the command
-    cmd="$base_cmd --config configs/models/qwen_2_audio_7b.yaml configs/datasets/short_zh/$cfg"
-    echo "$cmd"
-    $cmd
+for model_cfg in "${MODEL_CONFIGS[@]}"; do
+    for dataset_cfg in "${DATASET_CONFIGS[@]}"; do
+        # Execute the command
+        cmd="$base_cmd --config configs/models/$model_cfg configs/datasets/short_zh/$dataset_cfg"
+        echo "$cmd"
+        $cmd
+    done
 done
