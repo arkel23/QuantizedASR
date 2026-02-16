db0e53bd136595dcd42c022fd23c8fea363ef18c
ubuntu <calvinku1209@gmail.com>
2026-02-16 23:00:37 +0800

    updated defaults for long_en

--- scripts/eval_long_en.sh
+++ scripts/eval_long_en.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 DATASET_CONFIGS=(
-    # "openasr_longform_earnings22.yaml"
-    # "distil_rev16_whisper.yaml"
+    "openasr_longform_earnings21.yaml"
+    "openasr_longform_earnings22.yaml"
+    "openasr_longform_tedlium.yaml"
+    "distil_meanwhile.yaml"
+    "distil_rev16_whisper.yaml"
+    "distil_tedlium_longform.yaml"
+    "floras_dev.yaml"
+    "floras_test.yaml"
 )
     # "openasr_longform_earnings21.yaml"
     # "openasr_longform_earnings22.yaml"
@@ -15,26 +21,12 @@ DATASET_CONFIGS=(
 
 MODEL_CONFIGS=(
     "voxtral_small_24b.yaml"
+    "voxtral_mini_3b.yaml"
 )
 
-    # "whisper_tiny.yaml"
-    # "whisper_small.yaml"
-    # "whisper_base.yaml"
-    # "whisper_medium.yaml"
-    # "whisper_large.yaml"
-    # "whisper_large_v2.yaml"
-    # "whisper_large_v3.yaml"
-    # "whisper_large_v3_turbo.yaml"
 
-    # "lite_whisper_large_v3_acc.yaml"
-    # "lite_whisper_large_v3.yaml"
-    # "lite_whisper_large_v3_fast.yaml"
-    # "lite_whisper_large_v3_turbo_acc.yaml"
-    # "lite_whisper_large_v3_turbo.yaml"
-    # "lite_whisper_large_v3_turbo_fast.yaml"
-
-    # "voxtral_mini_3b.yaml"
-    # "voxtral_small_24b.yaml"
+# dont work with long-form using default processors and
+# not support long_form arguments that whisper uses
     # "qwen_25_omni_7b.yaml"
     # "qwen_2_audio_7b.yaml"
     # "qwen_2_audio_7b_instruct.yaml"
@@ -78,33 +70,32 @@ for model_cfg in "${MODEL_CONFIGS[@]}"; do
 done
 
 
-MODEL_CONFIGS=(
-    "whisper_large_v3.yaml"
-)
+# MODEL_CONFIGS=(
+#     "whisper_tiny.yaml"
+#     "whisper_small.yaml"
+#     "whisper_base.yaml"
+#     "whisper_medium.yaml"
+#     "whisper_large.yaml"
+#     "whisper_large_v2.yaml"
+#     "whisper_large_v3.yaml"
+#     "whisper_large_v3_turbo.yaml"
 
-    # "whisper_tiny.yaml"
-    # "whisper_small.yaml"
-    # "whisper_base.yaml"
-    # "whisper_medium.yaml"
-    # "whisper_large.yaml"
-    # "whisper_large_v2.yaml"
-    # "whisper_large_v3.yaml"
-    # "whisper_large_v3_turbo.yaml"
-
-    # "lite_whisper_large_v3_acc.yaml"
-    # "lite_whisper_large_v3.yaml"
-    # "lite_whisper_large_v3_fast.yaml"
-    # "lite_whisper_large_v3_turbo_acc.yaml"
-    # "lite_whisper_large_v3_turbo.yaml"
-    # "lite_whisper_large_v3_turbo_fast.yaml"
+#     "lite_whisper_large_v3_acc.yaml"
+#     "lite_whisper_large_v3.yaml"
+#     "lite_whisper_large_v3_fast.yaml"
+#     "lite_whisper_large_v3_turbo_acc.yaml"
+#     "lite_whisper_large_v3_turbo.yaml"
+#     "lite_whisper_large_v3_turbo_fast.yaml"
+# )
 
 
-# Iterate through all combinations
-for model_cfg in "${MODEL_CONFIGS[@]}"; do
-    for dataset_cfg in "${DATASET_CONFIGS[@]}"; do
-        # Execute the command
-        cmd="$base_cmd --config configs/models/$model_cfg configs/datasets/long_en/$dataset_cfg --wandb_save_figs"
-        echo "$cmd"
-        $cmd
-    done
-done
+
+# # Iterate through all combinations
+# for model_cfg in "${MODEL_CONFIGS[@]}"; do
+#     for dataset_cfg in "${DATASET_CONFIGS[@]}"; do
+#         # Execute the command
+#         cmd="$base_cmd --config configs/models/$model_cfg configs/datasets/long_en/$dataset_cfg --wandb_save_figs"
+#         echo "$cmd"
+#         $cmd
+#     done
+# done
