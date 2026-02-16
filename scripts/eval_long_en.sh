45eb73949677a0c7a91e82e4f2700ce423ec4e49
ubuntu <calvinku1209@gmail.com>
2026-02-16 22:11:55 +0800

    added --batch_size and --others args to control eval

--- scripts/eval_long_en.sh
+++ scripts/eval_long_en.sh
@@ -14,8 +14,6 @@ DATASET_CONFIGS=(
 
 MODEL_CONFIGS=(
     "voxtral_small_24b.yaml"
-    "qwen_25_omni_7b.yaml"
-    "whisper_large_v3.yaml"
 )
 
     # "whisper_tiny.yaml"
@@ -40,9 +38,65 @@ MODEL_CONFIGS=(
     # "qwen_2_audio_7b.yaml"
     # "qwen_2_audio_7b_instruct.yaml"
 
+batch_size='8'
+others=''
+
+VALID_ARGS=$(getopt  -o '' --long batch_size:,others: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    --batch_size)
+        batch_size=${2}
+        shift 2
+        ;;
+    --others)
+        others=${2}
+        shift 2
+        ;;
+    --) shift;
+        break
+        ;;
+  esac
+done
 
 
-base_cmd="python -m tools.evaluate --serial 400 --batch_size 16"
+base_cmd="python -m tools.evaluate --serial 400 --batch_size $batch_size $others"
+
+# Iterate through all combinations
+for model_cfg in "${MODEL_CONFIGS[@]}"; do
+    for dataset_cfg in "${DATASET_CONFIGS[@]}"; do
+        # Execute the command
+        cmd="$base_cmd --config configs/models/$model_cfg configs/datasets/long_en/$dataset_cfg --wandb_save_figs --max_new_tokens 8192"
+        echo "$cmd"
+        $cmd
+    done
+done
+
+
+MODEL_CONFIGS=(
+    "whisper_large_v3.yaml"
+)
+
+    # "whisper_tiny.yaml"
+    # "whisper_small.yaml"
+    # "whisper_base.yaml"
+    # "whisper_medium.yaml"
+    # "whisper_large.yaml"
+    # "whisper_large_v2.yaml"
+    # "whisper_large_v3.yaml"
+    # "whisper_large_v3_turbo.yaml"
+
+    # "lite_whisper_large_v3_acc.yaml"
+    # "lite_whisper_large_v3.yaml"
+    # "lite_whisper_large_v3_fast.yaml"
+    # "lite_whisper_large_v3_turbo_acc.yaml"
+    # "lite_whisper_large_v3_turbo.yaml"
+    # "lite_whisper_large_v3_turbo_fast.yaml"
+
 
 # Iterate through all combinations
 for model_cfg in "${MODEL_CONFIGS[@]}"; do
